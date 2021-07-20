c25597b4651a2de5b288b8ac391e0b033ab1e5f9
István Bozsó <bozso.istvan@epss.hu>
2021-07-20 13:25:22 +0200

    finished tmux stup script; adjusted tmux conf

--- tmux_workspace.sh
+++ tmux_workspace.sh
@@ -1,14 +1,53 @@
+workspaces="
+gotoolbox:$github/gotoolbox
+arman:$srht/arman
+shutil:$srht/shutil
+"
 
-detach() {
+workspace() {
+    local selector="fzf"
+    local selected=$(echo "${workspaces}" | "${selector}")
+
+    local name=$(echo "${selected}" |  cut -d ":" -f 1)
+    local path=$(echo "${selected}" |  cut -d ":" -f 2)
+
+    # detach from a tmux session if in one
+    tmws_detach
+
+    [ -n $(tmux list-sessions -F "#{session_name}" | \
+           grep -q "${name}") ] && tmws_attach "${name}" && return
+
+    tmws_create_session "${name}" "${path}"
+    tmws_setup_session
+    tmws_attach "${name}"
+}
+
+tmws_attach() {
+    local name="$1"
+    tmux attach -t "${name}"
+}
+
+tmws_detach() {
     tmux detach > /dev/null
 }
 
-workspace() {
-    local name="$1"
+tmws_create_session() {
+    local name="$1" path="$2"
+    cd "${path}"
+    tmux new-session -d -s "${name}"
+}
 
-    # detach from a tmux session if in one
-    detach
+tmws_setup_session() {
+    local path="$1" editor="nvim"
+    tmux rename-window "vim"
 
-    [ -z $(tmux list-sessions -F "#{session_name}" | \
-           grep -q "${name}") ] && return
+    tmux send-keys "${editor}" C-m
+    tmux split-window -h -p 35
+
+    tmux new-window -n fsys
+
+    tmux send-keys "fm" C-m
+    tmux split-window -h
+
+    tmux select-window -t 1
 }
