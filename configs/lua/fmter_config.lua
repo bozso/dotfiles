d5ae3aadb042ceda0f30e0038a4cf1030f476640
István Bozsó <bozso.istvan@epss.hu>
2022-01-26 11:29:40 +0100

    removed mypy lint; reverted back to black

--- configs/lua/fmter_config.lua
+++ configs/lua/fmter_config.lua
@@ -30,17 +30,8 @@ M.languages = {
 
     python = {
         {
-            formatCommand = "blackd-client",
+            formatCommand = "black --quiet -",
             formatStdin = true,
-        },
-        {
-            lintCommand = "mypy --show-column-numbers --ignore-missing-imports",
-            lintSource = "mypy",
-            lintFormats = {
-                "%f:%l:%c: %trror: %m",
-                "%f:%l:%c: %tarning: %m",
-                "%f:%l:%c: %tote: %m",
-            },
         },
     },
 
