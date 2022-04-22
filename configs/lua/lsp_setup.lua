118752245e27b6ca0b30c6da15348117b328398c
István Bozsó <bozso.istvan@epss.hu>
2022-04-22 21:27:50 +0200

    removed dead efm config code

--- configs/lua/lsp_setup.lua
+++ configs/lua/lsp_setup.lua
@@ -76,26 +76,6 @@ end
 
 local fmt = require "fmter_config"
 
--- lspconfig.efm.setup {
---     cmd = { "efm-langserver", "-logfile", "/tmp/efm.log" },
---     init_options = { documentFormatting = true },
---     flags = { debounce_text_changes = 150 },
---     filetypes = {
---         "python",
---         "lua",
---         "yaml",
---         "json",
---         "markdown",
---         "javascript",
---         "typescript",
---         "rust",
---     },
---     settings = {
---         rootMarkers = { ".git/", "Cargo.toml" },
---         languages = fmt.languages,
---     },
--- }
-
 lspconfig.gopls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
