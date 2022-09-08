049eb556033a0eb1e56cec23de151607c0022915
István Bozsó <bozso.istvan@epss.hu>
2022-09-08 10:19:01 +0200

    added repos and grouped them according to mode

--- configs/rss/github.lua
+++ configs/rss/github.lua
@@ -1,13 +1,89 @@
 local M = {}
 
-local all = { "releases", "commits" }
+local modes = {
+    "all",
+    "commits",
+    "releases",
+}
 
-M.github_repos = {
+local mode_arrays = {
+    all = { "releases", "commits" },
+    commits = { "commits" },
+    releases = { "releases" },
+}
+
+local with_all = {
     {
         user = "KAWAIYOO",
-        repo = { "LAMBDA" },
-        modes = all,
+        repos = { "LAMBDA" },
+    },
+    {
+        user = "matthew-gaddes",
+        repos = { "ICASAR" },
+    },
+    {
+        user = "gbaier",
+        repos = { "sarsim" },
+    },
+    {
+        user = "hdkarimi",
+        repos = { "awesome-gnss" },
+    },
+    {
+        user = "Fernerkundung",
+        repos = { "awesome-sentinel" },
+    },
+    {
+        user = "dbohdan",
+        repos = { "compilers-targeting-c" },
+    },
+    {
+        user = "andrei-markeev",
+        repos = { "ts2c" },
+    },
+    {
+        user = "mayfrost",
+        repos = { "guides" },
+    },
+    {
+        user = "sebbekarlsson",
+        repos = { "fuex" },
+    },
+    {
+        user = "dalyagergely",
+        repos = { "schupy" },
     },
 }
+
+local releases_only = {
+    {
+        user = "oven-sh",
+        repos = { "bun" },
+    },
+}
+
+local github_repos = {}
+
+local repos_with_modes = {
+    all = with_all,
+    releases = releases_only,
+}
+
+for _, mode in pairs(modes) do
+    local curr_repos = repos_with_modes[mode]
+    if curr_repos then
+        for _, repo in pairs(curr_repos) do
+            local curr_mode = mode_arrays[mode]
+            local curr = {
+                user = repo.user,
+                repos = repo.repos,
+                modes = curr_mode,
+            }
+            table.insert(github_repos, curr)
+        end
+    end
+end
+
+M.github_repos = github_repos
 
 return M
