654716c877000641ff563bee5e0eee86d53c4964
István Bozsó <bozso.istvan@epss.hu>
2022-09-20 11:27:26 +0200

    removed unused plugins

--- configs/lua/plugins.lua
+++ configs/lua/plugins.lua
@@ -48,8 +48,6 @@ return require("packer").startup(function(use)
         },
     }
 
-    use "mattn/emmet-vim"
-
     use {
         "earthly/earthly.vim",
         branch = "main",
@@ -60,6 +58,5 @@ return require("packer").startup(function(use)
     -- extra language packages
     -- commented out for now, using treesitter for syntax highlight
     -- use "teal-language/vim-teal"
-    -- use "ziglang/zig.vim"
     -- use "alaviss/nim.nvim"
 end)
