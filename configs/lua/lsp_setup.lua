c0783664d5bc3704d0b719f623a2e90cdb30f588
István Bozsó <bozso.istvan@epss.hu>
2022-10-21 10:06:48 +0200

    Disabled lua langserver.

--- configs/lua/lsp_setup.lua
+++ configs/lua/lsp_setup.lua
@@ -182,27 +182,27 @@ function M.setup_servers()
         },
     }
 
-    local luadev = require("lua-dev").setup {
-        library = {
-            vimruntime = true, -- runtime path
-            -- TODO: customize
-            -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
-            types = true,
-            -- installed opt or start plugins in packpath
-            plugins = true,
-            -- you can also specify the list of plugins to make available as a workspace library
-            -- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
-        },
-        -- enable this to get completion in require strings. Slow!
-        runtime_path = false,
-        -- pass any additional options that will be merged in the final lsp config
-        lspconfig = {
-            -- cmd = {"lua-language-server"},
-            -- on_attach = ...
-        },
-    }
-
-    lspconfig.sumneko_lua.setup(luadev)
+    -- local luadev = require("lua-dev").setup {
+    --     library = {
+    --         vimruntime = true, -- runtime path
+    --         -- TODO: customize
+    --         -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+    --         types = true,
+    --         -- installed opt or start plugins in packpath
+    --         plugins = true,
+    --         -- you can also specify the list of plugins to make available as a workspace library
+    --         -- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
+    --     },
+    --     -- enable this to get completion in require strings. Slow!
+    --     runtime_path = false,
+    --     -- pass any additional options that will be merged in the final lsp config
+    --     lspconfig = {
+    --         -- cmd = {"lua-language-server"},
+    --         -- on_attach = ...
+    --     },
+    -- }
+    --
+    -- lspconfig.sumneko_lua.setup(luadev)
 
     local efm = require "efm"
 
