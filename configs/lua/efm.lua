039565fb2c97733a10d751668d12823a6861648a
István Bozsó <bozso.istvan@epss.hu>
2022-10-24 11:38:42 +0200

    Added dmd config for efm.

--- configs/lua/efm.lua
+++ configs/lua/efm.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+-- local linters = require "efmls-configs.linters"
+
 -- /home/istvan/packages/src/sr.ht/pygeo/src/build.zig:44:17: error: use of undeclared identifier 'root'
 M.zig = {
     {
@@ -8,6 +10,17 @@ M.zig = {
             "%f:%l:%c: %rror: %m",
         },
     },
+}
+
+local linter = "dmd"
+local command = string.format("%s -color=off -vcolumns -o- -wi -c -", linter)
+
+M.dmd = {
+    prefix = linter,
+    lintCommand = command,
+    lintStdin = true,
+    lintFormats = { "%.%#(%l,%c): %trror: %m", "%.%#(%l,%c): %tarning: %m" },
+    rootMarkers = {},
 }
 
 return M
